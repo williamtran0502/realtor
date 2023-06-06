b2b018d5b93f5dbff733c3c0a697a4416699c525
williamtran0502 <tranngocduypro@gmail.com>
2023-06-06 16:25:13 +0700

    create sign in sign up forgot password components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,6 +24,7 @@ export default function Header() {
                     <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/offers") && "text-black border-b-red-500"}`} onClick={()=>navigate("/offers")} >Offers</li>
                     <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/sign-in") && "text-black border-b-red-500"}`} onClick={()=>navigate("/sign-in")}>Sign in</li>
                 </ul>
+                
             </div>
         </header>
     </div>
