e72c136a372275a8a91d092e0f173eaceed5cd05
williamtran0502 <tranngocduypro@gmail.com>
2023-06-06 12:26:38 +0700

    after creating pages and router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import './index.css';
-
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+import Offers from './pages/Offers';
+import ForgotPassword from './pages/ForgotPassword';
 
 function App() {
   return (
-    <div className="App">
-        <h1 className="text-3xl font-bold underline">
-          Hello World!
-        </h1>
-    </div>
+    <>
+      <Router>
+          <Routes>
+              <Route path="/" element={<Home/>} />
+              <Route path="/profile" element={<Profile/>} />
+              <Route path="/sign-in" element={<SignIn/>} />
+              <Route path="/sign-up" element={<SignUp/>} />
+              <Route path="/offers" element={<Offers/>} />
+              <Route path="/forgot-password" element={<ForgotPassword/>} />
+          </Routes>
+      </Router>
+    </>
   );
 }
 
