45b50b805581e12faab0ed5de0e914d62410b532
williamtran0502 <tranngocduypro@gmail.com>
2023-06-07 09:45:52 +0700

    complete forgot password

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -2,6 +2,8 @@ import React, {useState} from 'react'
 import {AiFillEyeInvisible, AiFillEye} from 'react-icons/ai'
 import { Link } from 'react-router-dom';
 import OAuth from '../components/OAuth';
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
+import {toast} from 'react-toastify'
 
 export default function SignIn() {
   const [showPassword, setShowPassword] = useState(false);
@@ -21,6 +23,18 @@ export default function SignIn() {
       setShowPassword(true);
     }
   }
+  async function onSubmit(e){
+    e.preventDefault();
+
+    try {
+      const auth = getAuth();
+      await sendPasswordResetEmail(auth, email);
+      toast.success("email was sent");
+    }catch(error){
+      toast.error("could not send rest password");
+    }
+
+  }
   return (
     <section>
         <h1 className='text-3xl text-center mt-6 font-bold'>Forgot Password</h1>
@@ -30,7 +44,7 @@ export default function SignIn() {
             <img src="https://media.istockphoto.com/id/507400394/vi/anh/ch%C3%ACa-kh%C3%B3a-v%C3%A0ng-v%C3%A0-c%C3%A2u-%C4%91%E1%BB%91.jpg?s=612x612&w=is&k=20&c=sxGYP4Kfr202QZSX0bYcYW6HLEeauRqRNLCLOaIKB3w=" alt="key" className='w-full rounded-2xl'/>
           </div>
           <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
-            <form >
+            <form onSubmit={onSubmit}>
               <input type="email" className='w-full' id="email" value={email} onChange={onChange} placeholder='Email address' className=" mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"/>
               
               <div className='flex justify-between whitespace-nowrap text-sm sm:text-lg'>
