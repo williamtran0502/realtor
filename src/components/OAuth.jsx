80358f832bfcb4ea87485f1286804f01b9ca9c58
williamtran0502 <tranngocduypro@gmail.com>
2023-06-07 09:12:31 +0700

    complete oauth

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,11 +1,40 @@
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
+import { doc, getDoc, serverTimestamp, setDoc } from 'firebase/firestore';
+import { db } from '../firebase';
 import React from 'react'
 import {FcGoogle} from 'react-icons/fc'
+import {toast} from 'react-toastify'
+import { useNavigate } from 'react-router';
 
 export default function OAuth() {
+  const navigate = useNavigate();
+  async function onGoogleClick(){
+    try{
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+ //update user in database 
+      // check user exists in database 
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+      if(!docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+      navigate("/");
+
+    }catch (error){
+      toast.error("could not authorize with google");
+    }
+  }
   return (
     <div>
-        <button className='flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 rounded uppercase text-sm font-medium hover:bg-red-800 active:b-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out'>
-            <FcGoogle className='text-2xl bg-white rounded-full mr-2'/>
+        <button onClick={onGoogleClick} type="button" className='flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 rounded uppercase text-sm font-medium hover:bg-red-800 active:b-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out'>
+            <FcGoogle className='text-2xl bg-white rounded-full mr-2' />
             continue with Google
         </button>
     </div>
