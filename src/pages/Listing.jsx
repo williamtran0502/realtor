f029a65a67400e48ec9d785556f1b97829fc58ed
williamtran0502 <tranngocduypro@gmail.com>
2023-06-08 16:16:11 +0700

    add listing infomations to listing page

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -6,7 +6,7 @@ import Spinner from '../components/Spinner';
 import {Swiper, SwiperSlide} from 'swiper/react'
 import SwiperCore, {EffectFade, Autoplay, Navigation, Pagination} from 'swiper'
 import "swiper/css/bundle"
-import {FaShare} from 'react-icons/fa'
+import {FaShare, FaMapMarkerAlt, FaBed, FaBath, FaParking, FaChair} from 'react-icons/fa'
 
 export default function Listing() {
     const params = useParams();
@@ -62,6 +62,64 @@ export default function Listing() {
                     <FaShare className='text-lg text-slate-500' />
                 </div>
                 {shareLinkCopied && <p className='fixed top-[9%] right-[3%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10'>Link Copied</p>}
+
+                <div className='m-4 flex flex-col md:flex-row max-w-6xl lg:mx-auto p-4 rounded-lg shadow-lg bg-white lg:space-x-5'>
+                    <div className='w-full h-[200px] lg-[400px]'>
+                        <p className='text-2xl font-bold mb-3 text-blue-900 '>
+                            {listing.name} - ${listing.offer 
+                            ? listing.discountedPrice 
+                                .toString()
+                                .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+                            : listing.regularPrice
+                                .toString()
+                                .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+                            }
+                            {listing.type ==="rent" ? " /month" : ""}
+                        </p>
+                        <p className='flex items-center mt-6 mb-3 font-semibold'>
+                            <FaMapMarkerAlt className='text-green-700 mr-1'/>
+                            {listing.address}
+                        </p>
+                        <div className='flex justify-start items-center space-x-4 w-[75%] '>
+                            <p className='bg-red-800 w-full max-w-[200px] rounded-md p-1 text-white text-center font-semibold shadow-md'>{listing.type === "rent" ? "Rent" : "Sale off:"}</p>
+                            <p>{listing.offer && (
+                                <p className='w-full max-w-[200px] bg-green-800 rounded-md p-1 text-white text-center font-semibold'>${
+                                    (listing.regularPrice - listing.discountedPrice)
+                                        .toString()
+                                        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+                                    }
+                                </p>
+                            )}
+                            </p>
+                        </div>
+                        <p className='mt-3 mb-3 '>
+                            <span className='font-semibold'>Description - </span>
+                            {listing.description}
+                        </p>
+                        <ul className='flex items-center space-x-2 sm:space-x-10 text-sm font-semibold'>
+                            <li className='flex items-center whitespace-nowarp'>
+                                <FaBed className='text-lg mr-1'/>
+                                {+listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : "1 Bed"}
+                            </li>
+                            <li className='flex items-center whitespace-nowarp'>
+                                <FaBath className='text-lg mr-1'/>
+                                {+listing.bathrooms > 1 ? `${listing.bedrooms} Bathrooms` : "1 Bath"}
+                            </li>
+                            <li className='flex items-center whitespace-nowarp'>
+                                <FaParking className='text-lg mr-1'/>
+                                {listing.parking ? "Parking Spot" : "No parking"}
+                            </li>
+                            <li className='flex items-center whitespace-nowarp'>
+                                <FaChair className='text-lg mr-1'/>
+                                {listing.furnished ? "Furnished" : "Not furnished"}
+                            </li>
+                            
+                        </ul>
+                    </div>
+                    <div className='bg-blue-300 w-full h-[200px] lg-[400px] z-10 overflow-x-hidden'>
+
+                    </div>
+                </div>
                 
             </main>
         )
