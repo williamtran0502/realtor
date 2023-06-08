1c8653d529514d24b86340a8744c0bc1e6a105aa
williamtran0502 <tranngocduypro@gmail.com>
2023-06-08 11:42:22 +0700

    add delete edit functions to my listing section

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,5 @@
 import { getAuth, updateProfile } from 'firebase/auth';
-import { doc, getDocs, query, updateDoc, where, collection, orderBy } from 'firebase/firestore';
+import { doc, getDocs, query, updateDoc, where, collection, orderBy, deleteDoc } from 'firebase/firestore';
 import React, {useState, useEffect} from 'react'
 import { useNavigate } from 'react-router';
 import {toast} from 'react-toastify';
@@ -65,6 +65,22 @@ export default function Profile() {
     }
     fetchUserListings();
   },[auth.currentUser.uid]);
+
+  async function onDelete(listingID){
+    if(window.confirm('Are you sure you want to delete')){
+       await deleteDoc(doc(db, "listings", listingID))
+       const updateListings = listings.filter(
+        (listing) => listing.id !== listingID
+       );
+       setListings(updateListings);
+       toast.success("successfully deleted");
+    }
+  }
+
+  function onEdit(listingID){
+    navigate(`/edit-listing/${listingID}`);
+  }
+
   return (
     <>
       <section className='max-w-6xl mx-auto flex justify-center items-center flex-col'>
@@ -104,7 +120,13 @@ export default function Profile() {
               <h2 className='text-2xl text-center font-semibold mb-6'>My Listings</h2>
               <ul className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 mt-6 mb-6'>
                 {listings.map((listing) =>(
-                  <ListingItem key={listing.id} id={listing.id} listing={listing.data}/>
+                  <ListingItem 
+                    key={listing.id} 
+                    id={listing.id} 
+                    listing={listing.data}
+                    onDelete={() => onDelete(listing.id)}
+                    onEdit={() => onEdit(listing.id)}
+                  />
                 ))}
               </ul>
           </>
