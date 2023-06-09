7690fb34a8d0222156b591c9cfd01b925aeb5f91
williamtran0502 <tranngocduypro@gmail.com>
2023-06-09 09:35:00 +0700

    complete map using leaflet

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -9,6 +9,7 @@ import SwiperCore, {EffectFade, Autoplay, Navigation, Pagination} from 'swiper'
 import "swiper/css/bundle"
 import {FaShare, FaMapMarkerAlt, FaBed, FaBath, FaParking, FaChair} from 'react-icons/fa'
 import { getAuth } from 'firebase/auth';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 
 export default function Listing() {
     const params = useParams();
@@ -128,9 +129,22 @@ export default function Listing() {
                         {contactLandlord && <Contact userRef={listing.userRef} listing={listing}/>}
                         
                     </div>
-                    <div className='bg-blue-300 w-full h-[200px] lg-[400px] z-10 overflow-x-hidden'>
-                            
-                    </div>
+                    {/* {listing.geolocation.lat && listing.geolocation.lng && ( */}
+                        <div className='w-full h-[200px] lg:h-[400px] z-10 overflow-x-hidden mt-6 md:mt-0 md:ml-2'>
+                            <MapContainer center={[listing.geolocation.lat, listing.geolocation.lng]} zoom={13} scrollWheelZoom={false} style={{height: "100%", width: "100%"}}>
+                                <TileLayer
+                                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                                />
+                                <Marker position={[listing.geolocation.lat, listing.geolocation.lng]}>
+                                <Popup>
+                                    A pretty CSS3 popup. <br /> Easily customizable.
+                                </Popup>
+                                </Marker>
+                            </MapContainer>
+                        </div>
+                    {/* )} */}
+                    
                 </div>
                 
             </main>
